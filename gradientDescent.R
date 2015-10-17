086ebd7d75cf59bc8a9a50ad75d35b0f6d134d9e
lraulier <lraulier@synotis.com>
2015-10-18 01:36:05 +0200

    essayer d'afficher les plot successif de la function de regression (hypothesis) avec differentes valeurs de theta (apres chaque step du gradient descent)

--- gradientDescent.R
+++ gradientDescent.R
@@ -8,6 +8,8 @@ gradientDescent <- function(X,y,theta,alpha,iteration){
     error <- (X %*% theta) - y
     # mise à jour des valeur de theta
     theta <- theta - (t(X) %*% error) %*% (alpha/dim(y)[1])
+    #plot(X[,2],(X %*% theta))
+    #Sys.sleep(2)
     J_history[i,1] <- computeCost(X,y,theta)
   }
   return(J_history)
