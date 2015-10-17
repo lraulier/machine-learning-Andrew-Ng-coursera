b94b4800006339d740548501cd5caa84983cd335
lraulier <lraulier@synotis.com>
2015-10-18 01:20:54 +0200

    finilize gradient descent function le cout de la function doit etre degressif

--- gradientDescent.R
+++ gradientDescent.R
@@ -1,9 +1,13 @@
 # use gradient descent to minimize the cost function
-gradientDescent <- function(X,y,theta,iteration){
+gradientDescent <- function(X,y,theta,alpha,iteration){
   #initialisation de la matrice history pour sauvegarder les
   # valeurs successive de costFunction
   J_history <- as.matrix(rep.int(0,iteration))
   for(i in 1:iteration){
+    # gradient descent step
+    error <- (X %*% theta) - y
+    # mise à jour des valeur de theta
+    theta <- theta - (t(X) %*% error) %*% (alpha/dim(y)[1])
     J_history[i,1] <- computeCost(X,y,theta)
   }
   return(J_history)
