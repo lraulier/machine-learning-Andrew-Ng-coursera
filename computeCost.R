d8798fc255bea90ce2d87c8a7a228a7333d6a4ae
lraulier <lraulier@synotis.com>
2015-10-18 00:37:28 +0200

    cost function = 32.07

--- computeCost.R
+++ computeCost.R
@@ -1,4 +1,7 @@
-# compute cost funtion
+#computeCost <- function(problem.data.X.matrice, problem.data.y.matrice, problem.data.initial.theta){
+#  return(sum(((problem.data.X %*% problem.data.initial.theta) - problem.data.y.matrice)^2))
+#}
 computeCost <- function(X, y, theta){
-  
+  return(sum(((X %*% theta) - y)^2)/(2 * dim(y)[1]))
 }
+
