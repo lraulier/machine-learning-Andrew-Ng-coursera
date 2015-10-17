ad1c558c2a49a1c8c0eaf7d58caf2b85ff99c6ab
lraulier <lraulier@synotis.com>
2015-10-18 01:11:52 +0200

    implementation de la boucle calculant les valeurs des thetas successifs minimiser par le gradient descent

--- gradientDescent.R
+++ gradientDescent.R
@@ -1,4 +1,10 @@
 # use gradient descent to minimize the cost function
-gradientDescent <- function(){
-  
+gradientDescent <- function(X,y,theta,iteration){
+  #initialisation de la matrice history pour sauvegarder les
+  # valeurs successive de costFunction
+  J_history <- as.matrix(rep.int(0,iteration))
+  for(i in 1:iteration){
+    J_history[i,1] <- computeCost(X,y,theta)
+  }
+  return(J_history)
 }
