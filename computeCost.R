eb900785f61a1e0080ffe2f94a3e728133eb1ac4
lraulier <lraulier@synotis.com>
2015-10-18 14:30:22 +0200

    comment

--- computeCost.R
+++ computeCost.R
@@ -2,6 +2,10 @@
 #  return(sum(((problem.data.X %*% problem.data.initial.theta) - problem.data.y.matrice)^2))
 #}
 computeCost <- function(X, y, theta){
+  # calcule le cout de la function (notre hypothese) 
+  # utilisation des moindres carrés
+  # pour des valeurs theta définies
+  # c'est en modifiant ces valeurs que l'on va reduire le cout de la fonction
   return(sum(((X %*% theta) - y)^2)/(2 * dim(y)[1]))
 }
 
